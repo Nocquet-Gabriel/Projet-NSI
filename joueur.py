0fda0d3b55d91899a1c281a41f749705908fc6b7
Nocquet-Gabriel <gabriel.nocquet@outlook.com>
2024-11-12 09:50:18 +0100

    Classe joueur : création attributs
    création attributs

--- joueur.py
+++ joueur.py
@@ -1,3 +1,9 @@
 class Joueur:
-    def __init__(self) -> None:
-        pass
+    def __init__(self,nom,age,diff) -> None:
+        self.nom=nom
+        self.age=age
+        self.nbPartiesGagnées=0
+        self.DifficulteChoisie=diff
+class Ordi:
+    def __init__(self,nom="Ordi",diff=1) -> None:
+        super().__init__(nom,diff)
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from case import Case
 from jeu import Jeu
-from joueur import Joueur
+from joueur import Joueur, Ordi
 from personnage import Personnage
 from plateau import Plateau
 from tkinter import*
