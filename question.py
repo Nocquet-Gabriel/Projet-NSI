6a4450edf3fafcc99ab54095c63baf0820b551a2
Nocquet-Gabriel <gabriel.nocquet@outlook.com>
2024-11-15 08:44:48 +0100

    Initialisation de la classe Joueur et la sous-classe Ordi + classe Question
    Modifications apportées au code pour l'initialisation de la classe Joueur ainsi que sa sous-classe Ordi, et création de la classe Question permettant de créer une question que l'on peut poser et lui associer les caractéristiques physiques lui étant associés pour l'avancement dans le jeu

---
+++ question.py
@@ -0,0 +1,5 @@
+class Question:
+    def __init__(self,question, caractéristiques:tuple) -> None:
+        self.question=question
+        self.reponse=None
+        self.caracteristiques=caractéristiques
