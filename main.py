de09c8f6d2d516f49112be83354b2391dc99f1ea
Elise-Carrouge <elise.carrouge@gmail.com>
2024-11-18 17:32:55 +0100

    Modification Page Accueil
    Rajout de la commande en commentaire pour modifier l'icon en haut a gauche de la fenetre l.13.
    Modification des noms des fonctions pour le bouton Multiplayer et modification de la fonction  Solo,
    notamment  une fois cliqué sur le bouton Solo une nouvelle fenetre s'affiche avec le message "Choose your difficulty".
     Et modification des boutons Solo et Multiplayer avec les bons noms de fonctions.

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ root = Tk()  # create root window
 root.title("Qui-est-ce ?")
 root.config(bg="dodgerblue")
 
+# Set the window icon
+#root.iconbitmap('path_to_icon.ico')
+
 # Create Frame widget
 left_frame = Frame(root, width=200, height=400)
 left_frame.grid(row=0, column=0, padx=10, pady=5)
@@ -18,30 +21,32 @@ left_frame.grid(row=0, column=0, padx=10, pady=5)
 tool_bar = Frame(left_frame, width=180, height=185)
 tool_bar.grid(row=2, column=0, padx=5, pady=5)
 
-# Load image to be "edited"
+# Load logo
 image = PhotoImage(file="QEC_Logo.png")
 original_image = image.subsample(3, 3)  # resize image using subsample
 Label(left_frame, image=original_image).grid(row=0, column=0, padx=5, pady=5)
 
-# Function to be called when the button is clicked
-def on_button_click():
-    print("Button clicked!")
+# Function to open a new window with a message for solo
+def on_button_solo():
+    new_window = Toplevel(root)
+    new_window.title("Solo")
+    Label(new_window, text="Choose your difficulty").pack(padx=20, pady=20)
 
 # Function to close the window
 def quit_app():
     root.quit()
 
 # Function to open a new window with a message
-def open_multiplayer_window():
+def open_button_multiplayer():
     new_window = Toplevel(root)
     new_window.title("Multiplayer")
     Label(new_window, text="Multiplayer not available yet").pack(padx=20, pady=20)
 
 # Create buttons and place them under the logo
-buttonSolo = Button(left_frame, text="Solo", command=on_button_click)
+buttonSolo = Button(left_frame, text="Solo", command=on_button_solo)
 buttonSolo.grid(row=1, column=0, padx=5, pady=5)
 
-buttonMultiplayer = Button(left_frame, text="Multiplayer", command=open_multiplayer_window)
+buttonMultiplayer = Button(left_frame, text="Multiplayer", command=open_button_multiplayer)
 buttonMultiplayer.grid(row=2, column=0, padx=5, pady=5)
 
 quit_button = Button(left_frame, text="Quit", command=quit_app)
