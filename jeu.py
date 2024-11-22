e6dfd81ca3f719d4764fdfb6f85612ac9b01f6b5
Nocquet-Gabriel <gabriel.nocquet@outlook.com>
2024-11-22 14:17:34 +0100

    Update jeu.py
    Ajout de tous les attributs de la classe jeu

--- jeu.py
+++ jeu.py
@@ -1,3 +1,6 @@
 class Jeu:
-    def __init__(self,difficulte:int) -> None:
+    def __init__(self,difficulte:int,joueur1,joueur2) -> None:
         self.difficulte = difficulte
+        self.joueur1=joueur1
+        self.joueur2=joueur2
+        self.plateau=None
