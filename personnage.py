7d458ac20a3ba4151eaea3e49e5e8df2cc155ccd
Nocquet-Gabriel <gabriel.nocquet@outlook.com>
2024-12-16 10:39:18 +0100

    création des attributs de la classe personnage
    création de tous les attributs utilisés par les questions qui seront posées dans le Qui-Est-ce. cette partie de code est sujette à modifications dans le futur

--- personnage.py
+++ personnage.py
@@ -1,3 +1,23 @@
 class Personnage:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, femme, cheveuxLongs, lunettes, moustache, cheveuxBoucles, cheveuxLisses, chapeau, yeuxBleus,
+                 yeuxMarrons, yeuxVerts, peauClaire, peauMate, BouclesOreilles, casquette, bonnet, souriant, jeune, vieux, serieux) -> None:
+        self.femme = femme
+        self.cheveuxLongs = cheveuxLongs
+        self.lunettes = lunettes
+        self.moustache = moustache
+        self.cheveuxBoucles = cheveuxBoucles
+        self.cheveuxLisses = cheveuxLisses
+        self.chapeau = chapeau
+        self.yeuxBleus = yeuxBleus
+        self.yeuxMarrons = yeuxMarrons
+        self.yeuxVerts = yeuxVerts
+        self.peauClaire = peauClaire
+        self.peauMate = peauMate
+        self.BouclesOreilles = BouclesOreilles
+        self.casquette = casquette
+        self.bonnet = bonnet
+        self.souriant = souriant
+        self.jeune = jeune
+        self.vieux = vieux
+        self.serieux = serieux
+        self.cache = False
