726f555a5354fa65d8eefd7e26c3c093772246cc
Nocquet-Gabriel <gabriel.nocquet@outlook.com>
2024-12-17 09:33:38 +0100

    ajout de l'instanciation des questions dans le main.py + adaptation de la classe question en circonstance
    modification des éléments à ajouter dans l'attribut caractéristiques (voir la docstring correspondante)

--- question.py
+++ question.py
@@ -1,5 +1,19 @@
 class Question:
-    def __init__(self,question, caractéristiques:tuple) -> None:
+    def __init__(self,question:str, caractéristiques:tuple) -> None:
+        """créée l'instanciation de classe question : possède 3 attributs : une 
+        question de type chaine de caractères, et les caractéristiques visées,
+        sous la forme d'un tuple de tuples de la forme 
+        (("caractéristique",effet),("caractéristique",effet))
+        l'effet indique si la réponse à la question 
+        va agir négativement 
+        (False si la réponse est True, True si la réponse est False),
+        positivement
+        (True si la réponse est True, False si la réponse est False).
+        ou semi-négativement (mot-clé = None)
+        (False si la réponse est True, mais  ne remplace pas la valeur 
+        existente si la réponse est False) (valeur = état personnage)
+        --> mène à des questions additionnelles
+        l'effet : True agit positivement et False 
+        agit négativement"""
         self.question=question
-        self.reponse=None
         self.caracteristiques=caractéristiques
