35b2c0afc5f1830206edbb456e7b6bbf1f2422d7
Nocquet-Gabriel <gabriel.nocquet@outlook.com>
2025-03-18 09:04:53 +0100

    jeuessai

--- joueur.py
+++ joueur.py
@@ -3,16 +3,30 @@ from personnage import Personnage
 from question import Question
 
 class Joueur:
-    def __init__(self,nom:str,age:int,personnage:str) -> None:
+    def __init__(self,nom:str,age:int,personnage:str,Ordi=False) -> None:
         self.nom=nom
         self.age=age
         self.nbPartiesGagnées=0
         self.personnageSelectionne=personnage
         self.gagne=False
+        self.ordi=Ordi
 
     def PoserQuestion(self, jeu, q:int):
-        question= jeu.questions[q]
-        print(question.question)
-        reponse = question.personnageCorrespond(jeu.persos[self.personnageSelectionne.lower()])
-        print(reponse)
-        
+        if self.ordi == False:
+            question= jeu.questions[q]
+            print(question.question)
+            reponse = question.personnageCorrespond(jeu.persos[self.personnageSelectionne.lower()])
+            print(reponse)
+        else:
+            question= jeu.questions[q]
+            print(question.question)
+            a=input("Votre Réponse (Oui/Non): ")
+            if a=="Oui" or a=="oui":
+                for key in jeu.persos.keys():
+                    perso = jeu.persos[key]
+                    question.answer(True,perso)
+            else:
+                for key,val in jeu.persos.items():
+                    perso = jeu.persos[key]
+                    question.answer(False,perso)
+                    
